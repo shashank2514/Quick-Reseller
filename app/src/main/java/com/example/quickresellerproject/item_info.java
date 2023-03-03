fc58bae00434ef4ce11e6e8558c6ad0147bff993
prudhvidhar18 <prudhvidharreddy3@gmail.com>
2023-03-03 13:37:20 -0600

    s545253:item_info activity and xml file updated

---
+++ app/src/main/java/com/example/quickresellerproject/item_info.java
@@ -0,0 +1,14 @@
+package com.example.quickresellerproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class item_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_info);
+    }
+}
