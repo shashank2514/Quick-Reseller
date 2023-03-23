aa452b2e17e7ad97bceb30fc7239b76c6b0dae86
Prudhvidhar Reddy Gopireddy <98135740+prudhvidhar18@users.noreply.github.com>
2023-03-23 11:23:54 -0500

    s545253 Created adminitem info java and xml file

---
+++ app/src/main/java/com/example/quickresellerproject/AdminItemInfo.java
@@ -0,0 +1,42 @@
+package com.example.quickresellerproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class AdminItemInfo extends AppCompatActivity {
+
+    TextView itemName,itemDescription,itemPrice;
+    ImageView itemImage;
+    Button contact_owner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_item_info);
+
+        itemName=findViewById(R.id.item_name_tv);
+        itemDescription=findViewById(R.id.description_tv);
+        itemPrice=findViewById(R.id.price_tv);
+        itemImage=findViewById(R.id.item_img_view);
+        contact_owner=findViewById(R.id.contact_owner_Btn);
+
+        Intent i=getIntent();
+        String name=i.getStringExtra("name");
+        String description=i.getStringExtra("description");
+        String price=i.getStringExtra("price");
+        String imgUrl=i.getStringExtra("imgUrl");
+        itemName.setText("Item: "+name);
+        itemDescription.setText(description);
+        itemPrice.setText("price: "+price+"$");
+        Picasso.with(getApplicationContext()).load(imgUrl).into(itemImage);
+
+
+    }
+}
