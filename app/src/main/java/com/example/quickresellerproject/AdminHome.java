6b88f44933a1eda4d066cb4a480f3e26316363a0
shashank2514 <shashankpasumarthi25@gmail.com>
2023-03-23 11:15:35 -0500

    S547089:AdminHome java file is updated

--- app/src/main/java/com/example/quickresellerproject/AdminHome.java
+++ app/src/main/java/com/example/quickresellerproject/AdminHome.java
@@ -9,16 +9,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.SearchView;
+import android.widget.ProgressBar;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class AdminHome extends AppCompatActivity {
 
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private AdminAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    public DatabaseReference databaseReference;
+    FirebaseUser firebaseUser;
+    FirebaseAuth auth;
+    ProgressBar progressBar;
     public ArrayList<AdminItem> bookitem;
 
 
@@ -28,13 +42,8 @@ public class AdminHome extends AppCompatActivity {
         setContentView(R.layout.activity_admin_home);
 
         bookitem=new ArrayList<>();
-        bookitem.add(new AdminItem("car","Descrption1","Price1","Date1"));
-        bookitem.add(new AdminItem("car","Descrption1","Price1","Date1"));
-        bookitem.add(new AdminItem("bike","Descrption1","Price1","Date1"));
-        bookitem.add(new AdminItem("tv","Descrption1","Price1","Date1"));
-        bookitem.add(new AdminItem("ac","Descrption1","Price1","Date1"));
-        bookitem.add(new AdminItem("fridge","Descrption1","Price1","Date1"));
-        bookitem.add(new AdminItem("Book","Descrption1","Price1","Date1"));
+        databaseReference= FirebaseDatabase.getInstance().getReference("Posts");
+
 
         mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setHasFixedSize(true);
@@ -43,6 +52,89 @@ public class AdminHome extends AppCompatActivity {
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+
+//        progressBar = findViewById(R.id.progress_bar);
+//        progressBar.setVisibility(View.VISIBLE);
+
+        auth=FirebaseAuth.getInstance();
+        firebaseUser =auth.getCurrentUser();
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    for(DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
+                        ReadWriteItemDetails post=dataSnapshot1.getValue(ReadWriteItemDetails.class);
+
+                        Date dt = new Date();
+                        SimpleDateFormat dateFormat;
+                        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                        String date=""+dateFormat.format(dt);
+
+                        bookitem.add(new AdminItem(post.name,post.description,post.price,date,post.Image_url));
+                    }
+
+                }
+//                progressBar.setVisibility(View.GONE);
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        mAdapter.setOnItemClickListener(new AdminAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position){
+                Intent i=new Intent(getApplicationContext(),AdminItemInfo.class);
+                i.putExtra("name",bookitem.get(position).getItemName());
+                i.putExtra("description",bookitem.get(position).getItemDescription());
+                i.putExtra("price",bookitem.get(position).getItemPrice());
+                i.putExtra("date",bookitem.get(position).getItemDate());
+                i.putExtra("imgUrl",bookitem.get(position).getImageUrl());
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu2,menu);
+
+
+        MenuItem AboutItem=menu.findItem(R.id.action_about_app);
+        MenuItem logoutItem=menu.findItem(R.id.action_logout);
+
+
+
+        AboutItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem menuItem) {
+                Intent i = new Intent(getApplicationContext(),About_Us.class);
+                startActivity(i);
+                return false;
+            }
+        });
+
+
+
+
+        logoutItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem menuItem) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i=new Intent(getApplicationContext(),login.class);
+                startActivity(i);
+                finish();
+                return false;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
