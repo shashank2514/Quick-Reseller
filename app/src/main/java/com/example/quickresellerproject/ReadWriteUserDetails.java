fa04e0435a6b01d865ee51956076f494ac25db9a
Nikhilchowdary Bonthu <s547077@nwmissouri.edu>
2023-03-22 22:00:10 -0500

    S547077 Added ReadWriteUserDetails java file

---
+++ app/src/main/java/com/example/quickresellerproject/ReadWriteUserDetails.java
@@ -0,0 +1,16 @@
+package com.example.quickresellerproject;
+
+public class ReadWriteUserDetails {
+    public String name,age,mobile,gender;
+    public boolean admin;
+
+    public ReadWriteUserDetails(String name, String age, String mobile, String gender,boolean admin) {
+        this.name = name;
+        this.age = age;
+        this.mobile = mobile;
+        this.gender = gender;
+        this.admin=admin;
+    }
+}
+
+
